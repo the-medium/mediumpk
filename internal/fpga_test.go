4c64ed39ae15166fa3f9a2b6610c25bb6515f0ea
sukill <ckdrms622@gmail.com>
2020-10-23 02:57:38 -0400

    [MDL-1912] clear c2h fifo when do reset

--- internal/fpga_test.go
+++ internal/fpga_test.go
@@ -26,39 +26,21 @@ func TestFPGA_Sign_CPU_Verify(t *testing.T) {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	assert.NoError(t, err)
 
-	d := privateKey.D.Bytes()
-	d32 := make([]byte, 32)
-	copy(d32[32-len(d):], d[:])
-
-	// generate random k
-	k32 := make([]byte, 32)
-	rand.Read(k32)
-
 	// get hashed message
 	msg := []byte("Hello World")
 	hasher := sha256.New()
 	hasher.Write(msg)
 	h32 := hasher.Sum(nil)
 
-	// userctx
-	userctx := 0xabc
-
-	// create buffer for sign request
-	reqBuf := make([]byte, SignRequestSize)
-
-	// fill parameters into buffer
-	binary.BigEndian.PutUint64(reqBuf[0:8], 12297829379609722880)
-	binary.BigEndian.PutUint64(reqBuf[8:16], uint64(userctx))
-	i := 16
-	i += copy(reqBuf[i:], d32)
-	i += copy(reqBuf[i:], k32)
-	i += copy(reqBuf[i:], h32)
-
 	// newfpgadevice
 	dev, err := NewFPGADevice(0)
 	assert.NoError(t, err)
 	assert.NotNil(t, dev)
 
+	// userctx
+	userctx := 0xab
+	reqBuf, err := genSignSet(privateKey, h32, userctx)
+	assert.NoError(t, err)
 	// send request
 	err = dev.Request(reqBuf)
 	assert.NoError(t, err)
@@ -85,58 +67,14 @@ func TestFPGA_Sign_CPU_Verify(t *testing.T) {
 }
 
 func TestCPU_Sign_FPGA_Verify(t *testing.T) {
-	// generate private key
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	assert.NoError(t, err)
-
-	// get hashed message
-	msg := []byte("Hello World")
-	hasher := sha256.New()
-	hasher.Write(msg)
-	h32 := hasher.Sum(nil)
-
-	// sign with cpu
-	_r, _s, err := ecdsa.Sign(rand.Reader, privateKey, h32)
-	assert.NoError(t, err)
-	assert.NotNil(t, _r)
-	assert.NotNil(t, _s)
-
-	// set qx32, qy32, r32, s32
-	qx := privateKey.PublicKey.X.Bytes()
-	qy := privateKey.PublicKey.Y.Bytes()
-	r := _r.Bytes()
-	s := _s.Bytes()
-
-	qx32 := make([]byte, 32)
-	qy32 := make([]byte, 32)
-	r32 := make([]byte, 32)
-	s32 := make([]byte, 32)
-
-	copy(qx32[32-len(qx):], qx[:])
-	copy(qy32[32-len(qy):], qy[:])
-	copy(r32[32-len(r):], r[:])
-	copy(s32[32-len(s):], s[:])
-
-	// userctx
-	userctx := 0xabc
-
-	// create buffer for verify request
-	reqBuf := make([]byte, VerifyRequestSize)
-
-	// fill parameters into buffer
-	binary.BigEndian.PutUint64(reqBuf[0:8], 13527612317570695168)
-	binary.BigEndian.PutUint64(reqBuf[8:16], uint64(userctx))
-	i := 16
-	i += copy(reqBuf[i:], qx32)
-	i += copy(reqBuf[i:], qy32)
-	i += copy(reqBuf[i:], r32)
-	i += copy(reqBuf[i:], s32)
-	i += copy(reqBuf[i:], h32)
-
 	// newfpgadevice
 	dev, err := NewFPGADevice(0)
 	assert.NoError(t, err)
 	assert.NotNil(t, dev)
+
+	userctx := 0xabc
+	reqBuf, err := genVerifySet(userctx)
+	assert.NoError(t, err)
 
 	// send verify request
 	err = dev.Request(reqBuf)
@@ -184,8 +122,23 @@ func TestFPGADevice_Reset(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, dev)
 
+	userctx := 0xabc
+	reqBuf, err := genVerifySet(userctx)
+	assert.NoError(t, err)
+
+	// send verify request (fill C2H FIFO)
+	for i := 1; i < 10; i++ {
+		err = dev.Request(reqBuf)
+		assert.NoError(t, err)
+	}
+
+	// Clear C2H FIFO
 	err = dev.Reset()
 	assert.NoError(t, err)
+
+	// Polling Empty C2H FIFO => Input/Output Error
+	_, err = dev.Poll()
+	assert.Error(t, err)
 
 	err = dev.Close()
 	assert.NoError(t, err)
@@ -203,4 +156,81 @@ func TestFPGADevice_Version(t *testing.T) {
 
 	err = dev.Close()
 	assert.NoError(t, err)
+}
+
+func genSignSet(privateKey *ecdsa.PrivateKey, h32 []byte, userctx int) ([]byte, error) {
+
+	d := privateKey.D.Bytes()
+	d32 := make([]byte, 32)
+	copy(d32[32-len(d):], d[:])
+
+	// generate random k
+	k32 := make([]byte, 32)
+	rand.Read(k32)
+
+	// create buffer for sign request
+	reqBuf := make([]byte, SignRequestSize)
+
+	// fill parameters into buffer
+	binary.BigEndian.PutUint64(reqBuf[0:8], 12297829379609722880)
+	binary.BigEndian.PutUint64(reqBuf[8:16], uint64(userctx))
+	i := 16
+	i += copy(reqBuf[i:], d32)
+	i += copy(reqBuf[i:], k32)
+	i += copy(reqBuf[i:], h32)
+
+	return reqBuf, nil
+}
+
+func genVerifySet(userctx int) ([]byte, error) {
+	// generate private key
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// get hashed message
+	msg := []byte("Hello World")
+	hasher := sha256.New()
+	hasher.Write(msg)
+	h32 := hasher.Sum(nil)
+
+	// sign with cpu
+	_r, _s, err := ecdsa.Sign(rand.Reader, privateKey, h32)
+	if err != nil {
+		return nil, err
+	}
+
+	// set qx32, qy32, r32, s32
+	qx := privateKey.PublicKey.X.Bytes()
+	qy := privateKey.PublicKey.Y.Bytes()
+	r := _r.Bytes()
+	s := _s.Bytes()
+
+	qx32 := make([]byte, 32)
+	qy32 := make([]byte, 32)
+	r32 := make([]byte, 32)
+	s32 := make([]byte, 32)
+
+	copy(qx32[32-len(qx):], qx[:])
+	copy(qy32[32-len(qy):], qy[:])
+	copy(r32[32-len(r):], r[:])
+	copy(s32[32-len(s):], s[:])
+
+	// userctx
+
+	// create buffer for verify request
+	reqBuf := make([]byte, VerifyRequestSize)
+
+	// fill parameters into buffer
+	binary.BigEndian.PutUint64(reqBuf[0:8], 13527612317570695168)
+	binary.BigEndian.PutUint64(reqBuf[8:16], uint64(userctx))
+	i := 16
+	i += copy(reqBuf[i:], qx32)
+	i += copy(reqBuf[i:], qy32)
+	i += copy(reqBuf[i:], r32)
+	i += copy(reqBuf[i:], s32)
+	i += copy(reqBuf[i:], h32)
+
+	return reqBuf, nil
 }
